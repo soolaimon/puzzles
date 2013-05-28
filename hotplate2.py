19c62e930bf27157250e950fd3885fc9bcaf7222
Dave Nelson <davidicus21@gmail.com>
2013-05-27 19:35:30 -0600

    Replaced all tabs with four spaces for consistency.

--- hotplate2.py
+++ hotplate2.py
@@ -10,8 +10,10 @@ hot_plate = []
 test = []
 
 for i in range(0, plates):
-	hot_plate.append(0.0)
-	test.append(0.0)
+    hot_plate.append(0.0)
+    test.append(0.0)
+
+
 
 
 middle_1 = (plates / 2) + (column_amount / 2) 
@@ -30,71 +32,71 @@ hot_plate[middle_4] = 100.0
 def heat_up(a):
 
 
-	old_cell = hot_plate[a]
+    old_cell = hot_plate[a]
 
-	count = 0
-	total = 0
+    count = 0
+    total = 0
 
-	if hot_plate[a - column_amount] is not None:
-		total += hot_plate[a - 10]
-		count += 1
+    if hot_plate[a - column_amount] is not None:
+        total += hot_plate[a - 10]
+        count += 1
 
-	if hot_plate[a - column_amount] is not None:
-		total += hot_plate[a - 10]
-		count += 1
+    if hot_plate[a - column_amount] is not None:
+        total += hot_plate[a - 10]
+        count += 1
 
-	if hot_plate[a - 1] is not None:
-		total += hot_plate[a - 1]
-		count += 1
+    if hot_plate[a - 1] is not None:
+        total += hot_plate[a - 1]
+        count += 1
 
-	if hot_plate [a + 1] is not None:
-		total += hot_plate [a + 1]
-		count += 1
+    if hot_plate [a + 1] is not None:
+        total += hot_plate [a + 1]
+        count += 1
 
 
-	avg = total / count
+    avg = total / count
 
-	new_cell = math.ceil(avg * 100) / 100
-	hot_plate[a] = new_cell
+    new_cell = math.ceil(avg * 100) / 100
+    hot_plate[a] = new_cell
 
-	test_cell = new_cell - old_cell
-	test[a] = test_cell
-	stop = all(x == 0 for x in test)
+    test_cell = new_cell - old_cell
+    test[a] = test_cell
+    stop = all(x == 0 for x in test)
 
-	if r != 0 and stop == True:
+    if r != 0 and stop == True:
 
-		print "Turns:", r	
-		for i, cell in enumerate(hot_plate):
-			sys.stdout.write(" %r" % cell)
-			if 0 == (i + 1) % column_amount:
-				sys.stdout.write(" \n ")
+        print "Turns:", r    
+        for i, cell in enumerate(hot_plate):
+            sys.stdout.write(" %r" % cell)
+            if 0 == (i + 1) % column_amount:
+                sys.stdout.write(" \n ")
 
-		sys.exit(0)
+        sys.exit(0)
 
 
 for r in range(0, 1000):
-	for a, i in enumerate(hot_plate):
+    for a, i in enumerate(hot_plate):
 
-		# The four cells in the center must stay at 100
+        # The four cells in the center must stay at 100
 
-		if a == middle_1 or \
-	   a == middle_2 or \
-	   a == middle_3 or \
-	   a == middle_4:
-	   
-			continue
+        if a == middle_1 or \
+       a == middle_2 or \
+       a == middle_3 or \
+       a == middle_4:
+       
+            continue
 
-		# The four corner squares must stay at 0
+        # The four corner squares must stay at 0
 
-		if a == 0  or \
-	   a == column_amount - 1  or \
-	   a == plates - column_amount or \
-	   a == plates - 1:
-	    	
-			continue
+        if a == 0  or \
+       a == column_amount - 1  or \
+       a == plates - column_amount or \
+       a == plates - 1:
+            
+            continue
 
-		heat_up(a)
+        heat_up(a)
 
 
 
-		
+        
